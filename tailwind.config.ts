b4f35782354084f78d1cacff191856cc41a32e1c
pannatorn <bong.pannatorn@gmail.com>
2024-12-09 13:06:58 +0700

    style : add more spacing

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,12 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      spacing: {
+        '25': '6.25rem', // 100px
+        '30': '7.5rem',  // 120px
+        '35': '8.75rem', // 140px
+        '40': '10rem',   // 160px
+      }
     },
   },
   plugins: [],
